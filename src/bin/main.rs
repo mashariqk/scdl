51610f427f571f3cb00f82258e17a0621b8e5e3f
Mashariq Khan <mashariq@outlook.com>
2022-06-15 13:19:32 -0500

    one set dl

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use error_chain::error_chain;
 use scdl::configuration::get_configuration;
 use std::fs::File;
-use std::io::{copy, Write};
+use std::io::Write;
 
 error_chain! {
     foreign_links {
@@ -18,24 +18,31 @@ async fn main() -> Result<()> {
     let m_prepend = &config.mod_nm[..2];
     let m_append = config.mod_append;
     let joiner = config.joiner;
-    let file_number = 1;
-    let target = format!(
-        "{}{:03}/{}{}{}{}{:03}.{}",
-        url1, &set_nm, m_prepend, m_append, set_nm, joiner, file_number, config.file_suffix
-    );
     let client = reqwest::Client::new();
-    let res = client.post(target).send().await?;
-    let mut dest = {
-        let fname = res
-            .url()
-            .path_segments()
-            .and_then(|segments| segments.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("tmp.jpg");
-        println!("File name is {}", &fname);
-        File::create(fname)?
-    };
-    let buf = res.bytes().await?;
-    dest.write(buf.as_ref())?;
+    for file_number in 1..300 {
+        let target = format!(
+            "{}{:03}/{}{}{}{}{:03}.{}",
+            url1, &set_nm, m_prepend, m_append, set_nm, joiner, file_number, config.file_suffix
+        );
+        let res = client.post(target).send().await?;
+        match res.status().as_u16() {
+            200 => {
+                let mut dest = {
+                    let fname = res
+                        .url()
+                        .path_segments()
+                        .and_then(|segments| segments.last())
+                        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                        .unwrap_or("tmp.jpg");
+                    println!("File name is {}", &fname);
+                    File::create(fname)?
+                };
+                let buf = res.bytes().await?;
+                dest.write(buf.as_ref())?;
+            }
+            _ => break,
+        }
+    }
+
     Ok(())
 }
--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
     // Add configuration values from a file named `configuration`. // It will look for any top-level file with an extension
     // that `config` knows how to parse: yaml, json, etc.
-    settings.merge(config::File::with_name("resources/configuration"))?;
+    settings.merge(config::File::with_name("configuration"))?;
     settings.try_into()
 }
 
