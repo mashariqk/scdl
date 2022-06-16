e227becfca91486204a8c9580e375127ff5e1126
Mashariq Khan <mashariq@outlook.com>
2022-06-16 14:41:29 -0500

    multithreaded download fixed threads

--- src/executor.rs
+++ src/executor.rs
@@ -15,24 +15,33 @@ pub async fn run() -> Result<()> {
     let start_set_nm = config.start_set_nm;
     let end_set_nm = config.end_set_nm;
     let num_threads = 3 as u16;
-    for mut set_nm in start_set_nm..=end_set_nm {
-        while end_set_nm - set_nm > num_threads {
-            let mut trds = vec![];
-            for i in set_nm..=(set_nm + num_threads) {
-                let x = tokio::spawn(async move {
-                    match downloader::download_set(i).await {
-                        Ok(()) => {}
-                        _ => {
-                            println!("Finished downloading set {}", set_nm);
-                        }
+    let mut counter = start_set_nm;
+    while end_set_nm - counter >= num_threads {
+        let mut trds = vec![];
+        for i in counter..(counter + num_threads) {
+            let x = tokio::spawn(async move {
+                match downloader::download_set(i).await {
+                    Ok(()) => {}
+                    _ => {
+                        println!("Finished downloading set {}", i);
                     }
-                });
-                trds.push(x);
+                }
+            });
+            trds.push(x);
+        }
+        for trd in trds {
+            trd.await.expect("TODO: panic message");
+        }
+        counter += num_threads;
+    }
+    if counter <= end_set_nm {
+        for i in counter..=end_set_nm {
+            match downloader::download_set(i).await {
+                Ok(()) => {}
+                _ => {
+                    println!("Finished downloading set {}", i);
+                }
             }
-            for trd in trds {
-                trd.await.expect("TODO: panic message");
-            }
-            set_nm = set_nm + num_threads;
         }
     }
     Ok(())
