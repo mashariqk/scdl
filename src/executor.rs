9b558a3f8192388a73c1265ac84d4382c67f9852
Mashariq Khan <mashariq@outlook.com>
2022-06-16 11:01:04 -0500

    added single threaded multiple set downloads

--- src/executor.rs
+++ src/executor.rs
@@ -12,7 +12,15 @@ error_chain! {
 
 pub async fn run() -> Result<()> {
     let config = get_configuration().expect("Config failure!");
-    let set_nm = config.set_nm;
-    downloader::download_set(set_nm).await?;
+    let start_set_nm = config.start_set_nm;
+    let end_set_nm = config.end_set_nm;
+    for set_nm in start_set_nm..=end_set_nm {
+        match downloader::download_set(set_nm).await {
+            Ok(()) => {}
+            _ => {
+                println!("Finished downloading set {}", set_nm);
+            }
+        }
+    }
     Ok(())
 }
