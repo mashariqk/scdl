6e63dc9dbf1e4399b5cfda3745c1bf5c3c574c93
Mashariq Khan <mashariq@outlook.com>
2022-06-16 14:33:44 -0500

    multithreaded download initial draft

--- src/executor.rs
+++ src/executor.rs
@@ -1,8 +1,6 @@
 use crate::configuration::get_configuration;
 use crate::downloader;
 use error_chain::error_chain;
-use futures::executor::block_on;
-use std::thread;
 
 error_chain! {
     foreign_links {
@@ -18,16 +16,21 @@ pub async fn run() -> Result<()> {
     let end_set_nm = config.end_set_nm;
     let num_threads = 3 as u16;
     for mut set_nm in start_set_nm..=end_set_nm {
-        while end_set_nm - set_nm < num_threads {
-            let mut threads = vec![];
+        while end_set_nm - set_nm > num_threads {
+            let mut trds = vec![];
             for i in set_nm..=(set_nm + num_threads) {
-                let x = thread::spawn(move || match block_on(downloader::download_set(i)) {
-                    Ok(()) => {}
-                    _ => {
-                        println!("Finished downloading set {}", set_nm);
+                let x = tokio::spawn(async move {
+                    match downloader::download_set(i).await {
+                        Ok(()) => {}
+                        _ => {
+                            println!("Finished downloading set {}", set_nm);
+                        }
                     }
                 });
-                threads.push(x);
+                trds.push(x);
+            }
+            for trd in trds {
+                trd.await.expect("TODO: panic message");
             }
             set_nm = set_nm + num_threads;
         }
