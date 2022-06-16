319fd16a034de69b9d1a73c66ee95beade81a617
Mashariq Khan <mashariq@outlook.com>
2022-06-16 10:39:24 -0500

    modularized set download

--- src/downloader.rs
+++ src/downloader.rs
@@ -10,6 +10,28 @@ error_chain! {
     }
 }
 
+pub async fn download_set(set_nm: u16) -> Result<()> {
+    let config = get_configuration().expect("Config failure!");
+    let url1 = config.get_url_to_append_set();
+    let mod_nm = config.mod_nm;
+    let m_prepend = &mod_nm[..2];
+    let m_append = config.mod_append;
+    let joiner = config.joiner;
+    let client = reqwest::Client::new();
+    let parent_dir_nm = format!("downloads/{}", &mod_nm);
+    std::fs::create_dir_all(&parent_dir_nm).expect("Cannot Create parent Directory!");
+    let set_dir_nm = format!("{}/{:03}", &parent_dir_nm, &set_nm);
+    std::fs::create_dir_all(&set_dir_nm).expect("Cannot Create set Directory!");
+    for file_number in 1..300 {
+        let target = format!(
+            "{}{:03}/{}{}{}{}{:03}.{}",
+            url1, &set_nm, m_prepend, m_append, set_nm, joiner, file_number, config.file_suffix
+        );
+        download(&target, &client, &set_dir_nm).await?;
+    }
+    Ok(())
+}
+
 pub async fn download(url: &String, client: &reqwest::Client, set_dir_nm: &String) -> Result<()> {
     let res = client.post(url).send().await?;
     match res.status().as_u16() {
--- src/executor.rs
+++ src/executor.rs
@@ -1,8 +1,6 @@
 use crate::configuration::get_configuration;
 use crate::downloader;
 use error_chain::error_chain;
-use std::fs::OpenOptions;
-use std::io::Write;
 
 error_chain! {
     foreign_links {
@@ -14,24 +12,7 @@ error_chain! {
 
 pub async fn run() -> Result<()> {
     let config = get_configuration().expect("Config failure!");
-    let url1 = config.get_url_to_append_set();
-    let mod_nm = config.mod_nm;
     let set_nm = config.set_nm;
-    let m_prepend = &mod_nm[..2];
-    let m_append = config.mod_append;
-    let joiner = config.joiner;
-    let client = reqwest::Client::new();
-    let parent_dir_nm = format!("downloads/{}", &mod_nm);
-    std::fs::create_dir_all(&parent_dir_nm).expect("Cannot Create parent Directory!");
-    let set_dir_nm = format!("{}/{:03}", &parent_dir_nm, &set_nm);
-    std::fs::create_dir_all(&set_dir_nm).expect("Cannot Create set Directory!");
-    for file_number in 1..300 {
-        let target = format!(
-            "{}{:03}/{}{}{}{}{:03}.{}",
-            url1, &set_nm, m_prepend, m_append, set_nm, joiner, file_number, config.file_suffix
-        );
-        downloader::download(&target, &client, &set_dir_nm).await?;
-    }
-
+    downloader::download_set(set_nm).await?;
     Ok(())
 }
